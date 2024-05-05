ebff115edbaef594a02a4d493cba9841f6939e30
Cesar <ccordero.007@gmail.cim>
2024-05-05 16:11:49 -0600

    Navbar Template for development First UI Kit component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
-import { Link } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
-import './styles/index.scss';
 import { AppRouter } from './providers/router';
+import './styles/index.scss';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const { theme, toogleTheme } = useTheme()
 
     return (
         <div className={classNames('app', { hovered: true, selected: true }, [theme, 'cls2', 'cls3'])}>
-            <button onClick={toogleTheme}>TOGGLE</button>
-            <Link to={'/'}>Main Page</Link>
-            <Link to={'/about'}>About Page</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toogleTheme}>TOGGLE</button>
         </div>
     )
 }
