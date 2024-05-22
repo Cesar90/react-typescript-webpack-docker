9ddf1f8a9c5ad2b10a6f07e6a5cf431f7d80b326
Cesar <ccordero.007@gmail.cim>
2024-05-21 20:40:45 -0600

    Modal Window and storybook. createPortal

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,28 +1,41 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
+import { useTranslation } from 'react-i18next';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { useCallback, useState } from 'react';
+import { Modal } from 'shared/ui/Modal';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            {/* <AppLink
-                theme={AppLinkTheme.SECONDARY}
-                to="/"
-                className={cls.mainLink}
+export const Navbar = ({ className }: NavbarProps) => {
+    const [isAuthModal, setIsAuthModal] = useState<boolean>(false);
+    const { t } = useTranslation();
+
+    const onCloseModal = useCallback(() => {
+        // setIsAuthModal((prev) => !prev);
+        setIsAuthModal(false);
+    }, []);
+
+    const onShowModal = useCallback(() => {
+        // setIsAuthModal((prev) => !prev);
+        setIsAuthModal(true);
+    }, []);
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <Button
+                theme={ThemeButton.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onShowModal}
             >
-                Main Page
-            </AppLink>
-            <AppLink
-                theme={AppLinkTheme.SECONDARY}
-                to="/about"
-            >
-                About
-            </AppLink> */}
-            /
+                Sing in
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onCloseModal}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur nihil voluptatem, rem eligendi dolore quam quae ducimus nobis voluptatum deleniti illum libero at, cumque molestias. Labore quas ipsum sapiente quidem!
+            </Modal>
         </div>
-    </div>
-);
+    )
+};
