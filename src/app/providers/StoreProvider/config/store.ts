9b0d92edd1785dde57707c0865210103eb4f73f7
Cesar <ccordero.007@gmail.cim>
2024-05-23 19:21:39 -0600

    Redux-toolkit. Entity, Test all levels selectors and slice

---
+++ src/app/providers/StoreProvider/config/store.ts
@@ -0,0 +1,33 @@
+import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
+
+import { counterReducer } from 'entities/counter';
+// import { userReducer } from 'entities/User';
+// import { loginReducer } from 'features/AuthByUsername';
+import { StateSchema } from './StateSchema';
+import { createReducerManager } from './reducerManager';
+
+export function createReduxStore(
+    initialState?: StateSchema,
+    asyncReducers?: ReducersMapObject<StateSchema>,
+) {
+    const rootReducer: ReducersMapObject<StateSchema> = {
+        ...asyncReducers,
+        counter: counterReducer,
+        // user: userReducer,
+        // loginForm: loginReducer,
+    };
+
+    const reducerManager = createReducerManager(rootReducer);
+
+    const store = configureStore<StateSchema>({
+        // reducer: rootReducer,
+        reducer: reducerManager.reduce,
+        devTools: __IS_DEV__,
+        preloadedState: initialState,
+    });
+
+    // @ts-ignore
+    store.reducerManager = reducerManager;
+
+    return store;
+}
---
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from 'react';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { StateSchema } from '../config/StateSchema';
+import { createReduxStore } from '../config/store';
+
+interface StoreProviderProps {
+    children?: ReactNode;
+    initialState?: DeepPartial<StateSchema>;
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+}
+
+export const StoreProvider = (props: StoreProviderProps) => {
+    const {
+        children,
+        initialState,
+        asyncReducers,
+    } = props;
+
+    const store = createReduxStore(
+        initialState as StateSchema,
+        asyncReducers as ReducersMapObject<StateSchema>,
+    );
+
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    );
+};
